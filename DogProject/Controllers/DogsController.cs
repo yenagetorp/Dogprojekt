7fee04dedd1e15122cf1855814c0de5ce67e3477
Yen Agetorp <yen.agetorp@gmail.com>
2019-01-30 19:42:41 +0100

    add dog proiect

---
+++ DogProject/Controllers/DogsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogProject.Controllers
+{
+    public class DogsController : Controller
+    {
+
+        DogsService dogsService = new DogsService();
+        [HttpGet]
+        [Route("")] //makes this as as  the home page.
+        public IActionResult Index()
+        {
+            var dogs = dogsService.GetAllDogs();
+            ViewBag.Dogs = dogs;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Dog dog)
+        {
+            dogsService.Add(dog);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //[HttpGet]
+        //public IActionResult Update()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var dog = dogsService.GetDogById(id);
+            ViewBag.Dog = dog;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Dog dog)
+        {
+           dogsService.UpdateDog(dog);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            dogsService.DeleteDog(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ DogProject/Models/DogsService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogProject.Models
+{
+    public class DogsService
+    {
+        static List<Dog> dogs = new List<Dog>
+        {
+            new Dog {Id=43, Name="Douglas", Age=7},
+            new Dog {Id=53, Name="Chap", Age=10},
+            new Dog {Id=62, Name="Bella", Age=5}
+
+        };
+
+        public void Add(Dog dog)
+        {
+            int newId;
+            if (dogs.Count==0)
+            {
+                newId = 1;
+            }
+            else
+            {
+            newId = dogs.Max(d => d.Id)+1;
+            }
+            dog.Id = newId;
+            dogs.Add(dog);
+        }
+
+        public Dog[] GetAllDogs()
+        {
+            return dogs.ToArray();
+        }
+
+        public Dog GetDogById(int id)
+        {
+            return dogs.SingleOrDefault(d => d.Id==id);
+        }
+
+        public void UpdateDog(Dog dog)
+        {
+           var oldDog= dogs.SingleOrDefault(d => d.Id == dog.Id);
+            if (oldDog!=null)
+            {
+                oldDog.Name = dog.Name;
+                oldDog.Age = dog.Age;
+            }
+        }
+
+        public void DeleteDog(int id)
+        {
+            dogs.RemoveAll(d => d.Id == id);
+        }
+
+    }
+}
